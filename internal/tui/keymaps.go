019fdb78696e7d5ca0535cb1aef2672c17da5137
Lanre Adelowo <yo@lanre.wtf>
2025-05-04 15:55:04 +0100

    clean up queues

--- internal/tui/keymaps.go
+++ internal/tui/keymaps.go
@@ -3,17 +3,11 @@ package tui
 import "github.com/charmbracelet/bubbles/key"
 
 type tuiKeyMaps struct {
-	toggleSpinner    key.Binding
-	toggleTitleBar   key.Binding
-	toggleStatusBar  key.Binding
-	togglePagination key.Binding
-	toggleHelpMenu   key.Binding
-	insertItem       key.Binding
-
 	down       key.Binding
 	up         key.Binding
 	quit       key.Binding
 	switchTabs key.Binding
+	blurTable  key.Binding
 }
 
 func newListKeyMap() *tuiKeyMaps {
@@ -34,29 +28,9 @@ func newListKeyMap() *tuiKeyMaps {
 			key.WithKeys("tab"),
 			key.WithHelp("tab", "Switch Tabs"),
 		),
-		insertItem: key.NewBinding(
-			key.WithKeys("a"),
-			key.WithHelp("a", "add item"),
-		),
-		toggleSpinner: key.NewBinding(
-			key.WithKeys("s"),
-			key.WithHelp("s", "toggle spinner"),
-		),
-		toggleTitleBar: key.NewBinding(
-			key.WithKeys("T"),
-			key.WithHelp("T", "toggle title"),
-		),
-		toggleStatusBar: key.NewBinding(
-			key.WithKeys("S"),
-			key.WithHelp("S", "toggle status"),
-		),
-		togglePagination: key.NewBinding(
-			key.WithKeys("P"),
-			key.WithHelp("P", "toggle pagination"),
-		),
-		toggleHelpMenu: key.NewBinding(
-			key.WithKeys("H"),
-			key.WithHelp("H", "toggle help"),
+		blurTable: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "Blur or Unblur"),
 		),
 	}
 }
